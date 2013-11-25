8246faed7e3ad091f4d4c5560e4f8ad3401c795b
cloudnull <kevin.carter@rackspace.com>
2013-11-24 19:45:25 -0600

    updated for stability

--- tribble/builder.py
+++ tribble/builder.py
@@ -18,18 +18,20 @@ import os
 import Queue
 import random
 import time
+import uuid
 
 from novaclient.v1_1 import client as nova_client
 
 
+MAX_FAULTS = 10
 POST_ACTION = """
 Done. If everything worked as expected, Here is what you have
 =============================================================
 
 The was done for account "%(username)s"
 
-Servers  = %(build_number)s
-Password = %(server_password)s
+Servers  = %(number)s
+Password = %(admin_pass)s
 Region = %(dc)s
 
 run: get-ips for the regions you just built
@@ -46,6 +48,22 @@ def arguments():
         ),
         epilog='GPLv3 Licensed.'
     )
+
+    name = argparse.ArgumentParser(add_help=False)
+    name.add_argument('--name',
+                      metavar='',
+                      help='Naming convention',
+                      required=True,
+                      default=None)
+
+    key = argparse.ArgumentParser(add_help=False)
+    key.add_argument('--key-name',
+                     metavar='',
+                     help=('Name of key to be injected into your Servers. If'
+                           ' the key name is not found and the key-location'
+                           ' is, a key will be created for you in NOVA and'
+                           ' injected.'),
+                     default=None)
 
     par.add_argument('-U',
                      '--username',
@@ -65,74 +83,65 @@ def arguments():
                      help='Your Region, use commas to separate regions',
                      required=True,
                      default=None)
-    par.add_argument('-N', '--name',
-                     metavar='',
-                     help='Naming convention for the server you want to build',
-                     required=False,
-                     default='Server')
-    par.add_argument('-I', '--image',
-                     metavar='',
-                     help='The servers Image Name or ID',
-                     required=False,
-                     default=None)
-    par.add_argument('-F', '--flavor',
-                     metavar='',
-                     type=int,
-                     help='The Servers Flavor Size, DEFAULT: %(default)s',
-                     required=False,
-                     default=2)
-    par.add_argument('-sp',
-                     '--server-password',
-                     metavar='',
-                     help=('Admin password for servers.'
-                           ' DEFAULT: "%(default)s"'),
-                     required=False,
-                     default='secrete')
-    par.add_argument('-bn',
-                     '--build-number',
-                     metavar='',
-                     type=int,
-                     help='Number of servers, DEFAULT: %(default)s',
-                     required=False,
-                     default=1)
     par.add_argument('--threads',
                      metavar='',
                      type=int,
                      help='Rate of Concurrency, DEFAULT: %(default)s',
                      required=False,
                      default=10)
-    par.add_argument('--key-name',
-                     metavar='',
-                     type=str,
-                     help='Name of key to be injected into your Servers',
-                     required=False,
-                     default=None)
     par.add_argument('--key-location',
                      metavar='',
-                     type=str,
                      help=('Location of the public key to be saved into'
                            ' nova for key injection into your servers.'
                            ' If your key-name does not exists and this'
                            ' variables file path is found the key will be'
                            ' created for you, DEFAULT: "%(default)s"'),
-                     required=False,
                      default='~/.ssh/id_rsa.pub')
-
     # Add a subparser
     subpar = par.add_subparsers()
 
     # Builder args
-    build = subpar.add_parser('build',
-                              help='Build instances')
-    build.set_defaults(build=True)
+    bld = subpar.add_parser('build',
+                            parents=[name, key],
+                            help='Build instances')
+    bld.set_defaults(build=True)
+    bld.add_argument('-i',
+                     '--image',
+                     metavar='',
+                     help='The servers Image Name or ID',
+                     required=False,
+                     default=None)
+    bld.add_argument('-f',
+                     '--flavor',
+                     metavar='',
+                     type=int,
+                     help='The Servers Flavor Size, DEFAULT: %(default)s',
+                     required=False,
+                     default=2)
+    bld.add_argument('-a',
+                     '--admin-pass',
+                     metavar='',
+                     help=('Admin password for servers.'
+                           ' DEFAULT: "%(default)s"'),
+                     required=False,
+                     default='secrete')
+    bld.add_argument('-n',
+                     '--number',
+                     metavar='',
+                     type=int,
+                     help='Number of servers, DEFAULT: %(default)s',
+                     required=False,
+                     default=1)
 
     # Destoryer args
-    destroy = subpar.add_parser('destroy',
-                                help='Destroy instances')
-    destroy.set_defaults(destroy=True)
+    dst = subpar.add_parser('destroy',
+                            parents=[name],
+                            help='Destroy instances')
+    dst.set_defaults(destroy=True)
 
     # Destoryer args
     get_ips = subpar.add_parser('get-ips',
+                                parents=[name],
                                 help='Get all server IPs')
     get_ips.set_defaults(get_ips=True)
 
@@ -225,86 +234,54 @@ class Clients(object):
             return client_type()
 
 
-def bob_the_builder(client, args, number, queue, fault=0):
-    """Build Instances."""
+def wait_for_active(client, number, queue, sid, status, tries=0):
+    """Wait for the instance to be active."""
 
-    def _kill(nova, server_id, num, work_queue):
+    def _kill_server(nova, server_id, num, work_queue):
         work_queue.put(num)
         time.sleep(1)
-        nova.servers.delete(server_id)
+        client_delete(client=nova, server_id=server_id)
 
-    kwargs = {
-        'name': '%s_%s_%s' % (args['name'], args['dc'], number),
-        'image': args['imageid'],
-        'flavor': args['flavor'],
-        'admin_pass': args['server_password']
-    }
+    time.sleep(10)
+    tries += 1
 
-    if args.get('key_name'):
-        if not client.keypairs.findall(name=args['key_name']):
-            if os.path.exists(args['key_location']):
-                keyfile = os.path.expanduser(args['key_location'])
-                with open(keyfile, 'rb') as key:
-                    client.keypairs.create(
-                        name=args['key_name'],
-                        public_key=key.read()
-                    )
-            else:
-                kwargs['key_name'] = args['key_name']
-        else:
-            kwargs['key_name'] = args['key_name']
+    # Retrieve the instance again so the status field updates
+    instance = client_server_get(client, server_id=sid)
+    status = instance.status
+    idnum = instance.id
 
-    instance = None
-    status = None
-
-    time.sleep(random.randrange(1, 5))
-    while True:
-        try:
-            instance = client.servers.create(**kwargs)
-            time.sleep(.1)
-            status = instance.status
-        except Exception, exc:
-            fault += 1
-            if instance is not None:
-                _kill(client, instance.id, number, queue)
-            print('EXCEPTION in build process: "%s" The application will'
-                  ' retry, Number of faults "%s"' % (exc, fault))
-            # Retry on Exception
-            if fault >= 10:
-                raise SystemExit('Too many fatal error happened while building'
-                                 ' your servers.')
-            else:
-                time.sleep(5)
-        else:
-            break
-
-    tryout = 100
-    tries = 0
-
-    while status != 'ACTIVE':
-        time.sleep(10)
-        tries += 1
-
-        # Retrieve the instance again so the status field updates
-        instance = client.servers.get(instance.id)
-        status = instance.status
-        idnum = instance.id
-
-        if status == 'ERROR':
-            print('%s is in ERROR and will be deleted. '
-                  'The job for server number %s will be requeued.'
-                  % (idnum, number))
-            _kill(client, idnum, number, queue)
-            break
-        elif tries >= tryout:
-            print('In buidling server "%s" we hit a the max attempts of %s we'
-                  ' will delete this instance and try again'
-                  % (idnum, tryout))
-            _kill(client, idnum, number, queue)
-            break
-    else:
+    if status == 'ACTIVE':
         print('Instance ID %s Name %s is ACTIVE'
               % (instance.id, instance.name))
+        return instance
+    elif status == 'ERROR':
+        print('%s is in ERROR and will be deleted. '
+              'The job for server number %s will be requeued.'
+              % (idnum, number))
+        _kill_server(client, idnum, number, queue)
+    elif tries >= MAX_FAULTS * 10:
+        print('In buidling server "%s" we hit a the max attempts of %s we'
+              ' will delete this instance and try again' % (idnum, MAX_FAULTS))
+        _kill_server(client, idnum, number, queue)
+    else:
+        wait_for_active(client, number, queue, sid, status, tries)
+
+
+def bob_the_builder(client, args, number, queue):
+    """Build Instances."""
+
+    _uuid = str(uuid.uuid4()).split('-')[0]
+    args['kwargs']['name'] = '%s_%s_%s' % (args['name'], args['dc'], _uuid)
+
+    # Build our instance
+    instance = client_create(client, build_hash=args['kwargs'])
+    if instance is None:
+        bob_the_builder(client, args, number, queue)
+    else:
+        instance_id = instance.id
+        instance_status = 'BUILD'
+        # Wait until the instance reports active
+        wait_for_active(client, number, queue, instance_id, instance_status)
 
 
 def basic_queue(iters=None):
@@ -378,35 +355,220 @@ def doerator(queue, client, args, job_action):
         job(client, args, wfile, queue)
 
 
-def runner(args, client, region, image):
+def job_check(args, client, tries=0):
+    all_servers = client_list(client)
+    if all_servers:
+        server_count = len(all_servers)
+
+        if server_count > args['number']:
+            diff = server_count - args['number']
+            print('Too many severs found, we should have "%s"'
+                  ' but we have "%s" The system will now normalize.'
+                  % (args['number'], server_count))
+            servers_ids = [server.id for server in client_list(client)
+                           if server.name.startswith(args['name'])]
+            for server_id in servers_ids[:diff]:
+                client_delete(client, server_id=server_id)
+    else:
+        if tries > MAX_FAULTS:
+            raise SystemExit('The system had too many failures, while'
+                             ' performing a job check')
+        else:
+            time.sleep(.5)
+            job_check(args, client)
+
+
+def client_list(client, fault=0):
+    """Return a list of Servers."""
+
+    try:
+        servers = client.servers.list()
+    except Exception as exc:
+        fault += 1
+        if fault > MAX_FAULTS:
+            print('ERROR IN CLIENT LIST: "%s", FAULT: %s' % (exc, fault))
+            raise SystemExit('The system had too many failures')
+        else:
+            client_list(client, fault=fault)
+    else:
+        if servers is not None:
+            return servers
+        else:
+            client_list(client)
+
+
+def client_delete(client, server_id, fault=0):
+    """Delete a server."""
+
+    try:
+        client.servers.delete(server_id)
+    except Exception as (exc, tb):
+        fault += 1
+        if fault > MAX_FAULTS:
+            print('ERROR IN CLIENT DELETE: "%s", FAULT: %s' % (exc, tb))
+            raise SystemExit('The system had too many failures')
+        else:
+            client_delete(client, server_id, fault=fault)
+
+
+def client_create(client, build_hash, fault=0):
+    """Return a list of Servers."""
+
+    server = None
+    try:
+        time.sleep(random.randrange(1, 5))
+        server = client.servers.create(**build_hash)
+        if server is None:
+            raise ValueError('Server Created returned a None Value.')
+    except Exception as exc:
+        fault += 1
+        if server is not None:
+            client_delete(client, server_id=server.id)
+
+        if fault > MAX_FAULTS:
+            print('ERROR IN CLIENT CREATE: "%s", FAULT: %s' % (exc, fault))
+            raise SystemExit('The system had too many failures')
+        else:
+            client_create(client, build_hash, fault=fault)
+    else:
+        return server
+
+
+def client_server_get(client, server_id, fault=0):
+    """Return a list of Servers."""
+
+    try:
+        server = client.servers.get(server_id)
+    except Exception as exc:
+        fault += 1
+        if fault > MAX_FAULTS:
+            print('ERROR IN CLIENT CREATE: "%s", FAULT: %s' % (exc, fault))
+            raise SystemExit('The system had too many failures')
+        else:
+            client_server_get(client, server_id, fault=fault)
+    else:
+        if server is not None:
+            return server
+        else:
+            fault += 1
+            client_server_get(client, server_id, fault=fault)
+
+
+def client_key_find(client, key_name, fault=0):
+    """See if a Key exists in Nova.
+
+    :return True||False:
+    """
+
+    try:
+        key = client.keypairs.findall(name=key_name)
+    except Exception as exc:
+        fault += 1
+        if fault > MAX_FAULTS:
+            print('ERROR IN CLIENT KEY NAME LOOKUP: "%s", FAULT: %s'
+                  % (exc, fault))
+            raise SystemExit('The system had too many failures')
+        else:
+            client_key_find(client, key_name, fault=fault)
+    else:
+        return key
+
+
+def client_key_create(client, key_name, public_key, fault=0):
+    """Create a Public Key for Server injection in NOVA."""
+
+    try:
+        client.keypairs.create(name=key_name, public_key=public_key)
+    except Exception as exc:
+        fault += 1
+        if fault > MAX_FAULTS:
+            print('ERROR IN CLIENT KEY CREATE: "%s", FAULT: %s' % (exc, fault))
+            raise SystemExit('The system had too many failures')
+        else:
+            client_key_create(client, key_name, public_key, fault=fault)
+
+
+def runner(args, region):
     """Run the application process from within the thread.
 
     :param
     """
-    if args.get('build') is True:
-        # Load the queue
-        queue = basic_queue(iters=range(args['build_number']))
+    creds = rax_creds(
+        user=args['username'],
+        apikey=args['password'],
+        region=region
+    )
+    # Get the client
+    nova = Clients(creds=creds).novaclient()
+    image = args.get('image')
+    if image is not None:
+        image_ids = [img.id for img in nova.images.list()
+                     if image in img.name or image == img.id]
+        if image_ids:
+            if len(image_ids) > 1:
+                raise SystemExit('We found more than one image with'
+                                 ' id/name of "%s" You are going to be'
+                                 ' more specific'
+                                 % image)
+            else:
+                image_id = image_ids[0]
+        else:
+            raise SystemExit('Image id/name "%s" was not found in Region'
+                             ' "%s" You may want to try using the image'
+                             ' name instead of the ID' % (image, region))
+    else:
+        image_id = None
 
-        args['imageid'] = image
+    if args.get('build') is True:
+        args['imageid'] = image_id
         args['dc'] = region
+
+        args['kwargs'] = {
+            'image': args['imageid'],
+            'flavor': args['flavor'],
+            'admin_pass': args['admin_pass']
+        }
+
+        if args.get('key_name'):
+            if not client_key_find(nova, key_name=args['key_name']):
+                key_path = os.path.expanduser(args['key_location'])
+                if os.path.exists(key_path):
+                    with open(key_path, 'rb') as key:
+                        client_key_create(
+                            nova,
+                            key_name=args['key_name'],
+                            public_key=key.read()
+                        )
+                    args['kwargs']['key_name'] = args['key_name']
+            else:
+                args['kwargs']['key_name'] = args['key_name']
+
+        # Load the queue
+        queue = basic_queue(iters=range(args['number']))
 
         # Prep the threader
         worker_proc(job_action=bob_the_builder,
                     queue=queue,
-                    client=client,
+                    client=nova,
                     args=args,
                     job=doerator)
-
         queue.close()
+
+        # Check that we have as many servers as we specified
+        job_check(args, nova)
+
+        # Show some information
         print(POST_ACTION % args)
+
     elif args.get('destroy') is True:
-        servers = [server.id for server in client.servers.list()
+        servers = [server.id for server in client_list(nova)
                    if server.name.startswith(args['name'])]
         for server in servers:
-            client.servers.delete(server)
+            client_delete(nova, server_id=server)
+
     elif args.get('get_ips') is True:
         servers = [(server.status, server.accessIPv4)
-                   for server in client.servers.list()
+                   for server in client_list(nova)
                    if server.name.startswith(args['name'])]
         filename = '%s_server_ips' % region
         if os.path.exists(filename):
@@ -418,5 +580,6 @@ def runner(args, client, region, image):
                 if state == 'ACTIVE':
                     if ip:
                         ips.writelines('%s\n' % ip)
+
     else:
-        raise SystemExit('died because something bad happened.')
+        raise SystemExit('Died because something bad happened.')
--- tribble/executable.py
+++ tribble/executable.py
@@ -18,34 +18,8 @@ def execute():
     datacenters = user_args['region'].split(',')
     user_args['threads'] /= len(datacenters)
     for dc in datacenters:
-        creds = builder.rax_creds(
-            user=user_args['username'],
-            apikey=user_args['password'],
-            region=dc
-        )
-        # Get the client
-        nova = builder.Clients(creds=creds).novaclient()
-
-        _image = user_args['image']
-        if _image is not None:
-            image_ids = [img.id for img in nova.images.list()
-                         if _image in img.name or _image == img.id]
-            if image_ids:
-                if len(image_ids) > 1:
-                    raise SystemExit('We found more than one image with'
-                                     ' id/name of "%s" You are going to be'
-                                     ' more specific'
-                                     % _image)
-                else:
-                    image_id = image_ids[0]
-            else:
-                raise SystemExit('Image id/name "%s" was not found in Region'
-                                 ' "%s" You may want to try using the image'
-                                 ' name instead of the ID' % (_image, dc))
-        else:
-            image_id = None
         # Start Action Thread
-        action = (user_args, nova, dc, image_id)
+        action = (user_args, dc)
         multiprocessing.Process(target=builder.runner, args=action).start()
 
 
